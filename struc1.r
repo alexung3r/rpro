257d1c56ed1feea149d43f95b5a8509d1e602e91
alexung3r <140854911+alexung3r@users.noreply.github.com>
2023-08-31 16:03:18 +0200

    Update struc1.r

--- struc1.r
+++ struc1.r
@@ -29,3 +29,21 @@ mean_unemployment <- mean(data$Estimated.Unemployment.Rate...., na.rm = TRUE)
 data_imputed <- data
 data_imputed$Estimated.Unemployment.Rate....[is.na(data_imputed$Estimated.Unemployment.Rate....)] <- mean_unemployment
 
+
+#compute a logistic regression
+# Filter out unwanted variables
+titanic_filtered <- titanic %>%
+  select(Survived, Pclass, Age, SibSp, Parch)
+head(titanic_filtered)
+# Split the data into training and testing sets
+set.seed(123)  # For reproducibility
+train_indices <- sample(nrow(titanic_filtered), 0.7 * nrow(titanic_filtered))
+train_data <- titanic_filtered[train_indices, ]
+test_data <- titanic_filtered[-train_indices, ]
+
+# Fit the logistic regression model
+log_model <- glm(Survived ~ Pclass + Age + SibSp + Parch, data = train_data, family = "binomial")
+
+# Print the summary of the model
+summary(log_model)
+
